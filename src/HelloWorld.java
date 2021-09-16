2e39254bade6fe1431563d8a6503e2e8d77c9e31
lizih <lizihuang@yahoo.com>
2021-09-16 16:21:12 -0400

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner myobj=new Scanner(System.in);
+		String A=myobj.nextLine();
+	//	String A="abca";
+		String B=A.replace("a","b");
+
+		String[] in={"0", "1", "2", "3"};
+		for (int i=3; i>=0; i--) {
+			System.out.print(in[i]+" ");
+		}
+
+      System.out.println(B);
+	}
+
+}
